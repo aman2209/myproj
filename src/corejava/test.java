4a4369c08526ff6091214c3957c8e666483e8e00
HP <HP@LAPTOP-8NR7DI7T>
2020-02-22 12:36:07 +0530

    by aman

---
+++ src/corejava/test.java
@@ -0,0 +1,20 @@
+package corejava;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class test {
+	
+	public static void main(String[] args) {
+		
+       List<Integer> l = new LinkedList<Integer>();
+          l.add(0, 2);
+          l.add(1, 6);
+          l.add(8, 5);
+		System.out.println(l.get(0));
+	}
+
+}
